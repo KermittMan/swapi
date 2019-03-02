dc7332e418081488502345f4547fbb66e20f3870
Kermitt <Mantilla.K.B@gmail.com>
2019-03-02 00:52:47 -0500

    Updates

---
+++ precache-manifest.977065e02e7810cf57c18b8f6ab9eaa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "721c924ec48708198afb2a0664196fb0",
+    "url": "/swapi/static/media/tiefighter.721c924e.jpg"
+  },
+  {
+    "revision": "5a32f8fe521c9a95db9d",
+    "url": "/swapi/static/js/runtime~main.5a32f8fe.js"
+  },
+  {
+    "revision": "e4eabf3979a692c65087",
+    "url": "/swapi/static/js/main.e4eabf39.chunk.js"
+  },
+  {
+    "revision": "395376288e1f753329fb",
+    "url": "/swapi/static/js/1.39537628.chunk.js"
+  },
+  {
+    "revision": "e4eabf3979a692c65087",
+    "url": "/swapi/static/css/main.0b64b4cc.chunk.css"
+  },
+  {
+    "revision": "395376288e1f753329fb",
+    "url": "/swapi/static/css/1.06290a50.chunk.css"
+  },
+  {
+    "revision": "466037f8fc9b9fb1575cd47f38efe855",
+    "url": "/swapi/index.html"
+  }
+];
